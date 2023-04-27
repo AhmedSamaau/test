f636655b85696484bcb54f22cd128fbb48b69b08
AhmedSamaau <128492587+AhmedSamaau@users.noreply.github.com>
2023-04-27 16:26:37 +0500

    Update syscall.h

--- syscall.h
+++ syscall.h
@@ -1,6 +1,21 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include "threads/synch.h"
+#include "threads/thread.h"
 void syscall_init (void);
+
+struct child_process {
+  int pid;
+  int load_status;
+  int wait;
+  int exit;
+  int status;
+  struct semaphore load_sema;
+  struct semaphore exit_sema;
+  struct list_elem elem;
+};
+
+
 
 #endif /* userprog/syscall.h */
