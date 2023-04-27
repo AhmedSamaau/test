49b32860627c4661f3e185ee8b9fb04480c4f65e
AhmedSamaau <128492587+AhmedSamaau@users.noreply.github.com>
2023-04-27 16:24:00 +0500

    Update syscall.c

--- syscall.c
+++ syscall.c
@@ -3,8 +3,37 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/malloc.h"
+#include "threads/synch.h"
+#include "threads/vaddr.h"
+#include "userprog/pagedir.h"
+#include "userprog/process.h"
+#include <user/syscall.h>
+#include "devices/input.h"
+#include "devices/shutdown.h"
+#include "filesys/file.h"
+#include "filesys/filesys.h"
+
+#define MAX_ARGS 3
 
 static void syscall_handler (struct intr_frame *);
+int add_file (struct file *file_name);
+void get_args (struct intr_frame *f, int *arg, int num_of_args);
+void syscall_halt (void);
+pid_t syscall_exec(const char* cmdline);
+int syscall_wait(pid_t pid);
+bool syscall_create(const char* file_name, unsigned starting_size);
+bool syscall_remove(const char* file_name);
+int syscall_open(const char * file_name);
+int syscall_filesize(int filedes);
+int syscall_read(int filedes, void *buffer, unsigned length);
+int syscall_write (int filedes, const void * buffer, unsigned byte_size);
+void syscall_seek (int filedes, unsigned new_position);
+unsigned syscall_tell(int fildes);
+void syscall_close(int filedes);
+void validate_ptr (const void* vaddr);
+void validate_str (const void* str);
+void validate_buffer (const void* buf, unsigned byte_size);
 
 void
 syscall_init (void) 
